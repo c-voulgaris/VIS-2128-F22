2718df07fc372acdac38bffd2369405d49afe02f
c-voulgaris <cvoulgaris@gsd.harvard.edu>
2022-10-11 11:32:07 -0400

    added leaflet example for assignment 6

--- a6-files/Examples/trees-in-parks.R
+++ a6-files/Examples/trees-in-parks.R
@@ -2,186 +2,73 @@ library(tidyverse)
 library(sf)
 library(tidycensus)
 library(here)
-library(ggspatial)
-library(ggthemes)
-library(gridExtra)
-library(grid)
 library(leaflet)
-library(tigris)
 
-# Define CA-state-plane
-CA_st_plane <- "+proj=lcc +lat_1=34.03333333333333 +lat_2=35.46666666666667 +lat_0=33.5 +lon_0=-118 +x_0=2000000 +y_0=500000.0000000002 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs"
+# Define State Plane
+MA_st_plane <- "+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000.0001016002 +y_0=750000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +to_meter=0.3048006096012192 +no_defs" 
 
+# Load Layers
+trees <- st_read("https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::trees.geojson") %>%
+  st_transform(MA_st_plane)
 
-city <- places(state = "CA") %>%
-  filter(NAME == "San Luis Obispo") %>%
-  st_transform(CA_st_plane)
-
-bbox <- st_bbox(city)
-
-
-# Load layers
-co_trails <- here("a1-files",
-                  "County_Trails.geojson") %>%
-  st_read() %>%
-  dplyr::select(TrailName, Length, LengthUnits)
-
-city_trails <- here("a2-files",
-                    "Examples",
-                    "Trail.geojson") %>%
-  st_read() %>%
-  rename(TrailName = Trail_Name,
-         Length = SLength) %>%
-  mutate(LengthUnits = "MILES") %>%
-  dplyr::select(TrailName, Length, LengthUnits)
-
-trails <- rbind(city_trails, co_trails) %>%
-  st_transform(CA_st_plane) %>%
-  st_filter(city)
-
-rta_stops <- here("a1-files",
-                  "bus-stops.csv") %>%
-  st_read(options=c("X_POSSIBLE_NAMES=stop_lon",
-                    "Y_POSSIBLE_NAMES=stop_lat")) %>%
-  mutate(`Transit Provider` = "San Luis Obispo RTA") 
-
-slo_stops <- here("a2-files",
-                  "Examples",
-                  "slo-stops.csv") %>%
-  st_read(options=c("X_POSSIBLE_NAMES=stop_lon",
-                    "Y_POSSIBLE_NAMES=stop_lat")) %>%
-  mutate(`Transit Provider` = "SLO Transit") %>%
-  dplyr::select(colnames(rta_stops)) 
-
-all_stops <- rbind(rta_stops, slo_stops) %>%
-  st_set_crs("WGS84") %>%
-  mutate(id = 
-           as.character(seq(1,
-                            length(rta_stops$stop_id) + 
-                              length(slo_stops$stop_id),
-                            by=1))) %>%
-  st_transform(CA_st_plane) %>%
-  st_filter(city)
-
-streets <- osmextract::oe_get("San Luis Obispo") %>%
-  st_transform(CA_st_plane) %>%
-  st_filter(city) %>%
-  filter(highway == "primary" |
-           highway == "secondary" |
-           highway == "motorway" |
-           highway == "tertiary" |
-           highway == "residential")
-  
-closest_stop <- st_nearest_feature(trails,
-                                   all_stops)
-
-trails <- trails %>%
-  mutate(dist_to_transit = st_distance(trails, all_stops[closest_stop,], by_element = TRUE)) %>%
-  mutate(dist_to_transit = as.numeric(dist_to_transit))
-
-stop_buffer <- all_stops %>%
-  st_buffer(dist = 1320) %>%
-  st_union
-
-trail_colors <- RColorBrewer::brewer.pal(7, "YlOrBr")
-
-ggplot() +
-  geom_sf(data = streets,
-          color = "gray") +
-  geom_sf(data = stop_buffer,
-          alpha = 0.2,
-          color = NA,
-          aes(fill = "Area within 1/4 mile of transit")) +
-  geom_sf(data = all_stops,
-          aes(shape = "Transit stop")) +
-  geom_sf(data = trails,
-          size = 1.2,
-          aes(color = dist_to_transit)) +
-  scale_color_gradientn(colors = rev(trail_colors),
-                        breaks = seq(0, 5280, by = 1320),
-                        labels = c("At transit stop",
-                                   "1/4 mile from transit",
-                                   "1/2 mile from transit",
-                                   "3/4 mile from transit",
-                                   "1 mile from transit"),
-                        limits = c(0, 6000),
-                        name = "Trails") +
-  scale_shape(name = "") +
-  scale_fill_manual(name = "", values = "gray") +
-  annotation_north_arrow(style = north_arrow_minimal,
-                         location = "tr") +
-  theme_map() +
-  theme(legend.background = element_rect(fill = NA),
-        legend.margin = margin(0,0,0,0),
-        plot.background = element_rect(fill = "white",
-                                       color = NULL)) +
-  guides(shape = guide_legend(order = 3),
-         col = guide_legend(order = 1),
-         fill = guide_legend(order = 2))
-
-ggsave(filename = here("a4-files", "Examples", "slo-trails.png"), 
-       width = 8.5, height = 8, units = "in", dpi = 300)
-
-wgs_trails <- trails %>%
+parks <- st_read("https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::open-space.geojson") %>%
+  st_transform(MA_st_plane) %>%
+  mutate(trees_in_park = lengths(st_intersects(., trees))) %>%
+  mutate(trees_per_acre = trees_in_park / ACRES) %>%
+  filter(ACRES > 0 & trees_in_park > 0) %>%
   st_transform("WGS84") %>%
-  mutate(popup_txt = paste0("<b>",
-                            TrailName,
-                            "</b><br>",
-                            formatC(dist_to_transit/5280, digits = 1, format = "f"),
-                            " miles from a transit stop")) %>%
-  mutate(miles = dist_to_transit / 5280)
+  mutate(popup_text = 
+           paste0("<b>",
+                  SITE_NAME,
+                  "</b><br>",
+                  formatC(ACRES, format = "f", digits = 2),
+                  " Acres<br>",
+                  trees_in_park,
+                  " trees<br>",
+                  formatC(trees_per_acre, format = "f", digits = 2),
+                  " trees per acre"))
 
-wgs_buffer <- stop_buffer %>%
-  st_transform("WGS84")
+my_ramp = colorRamp(c("pink", 
+                      "brown", 
+                      "orange", 
+                      "lightgreen", 
+                      "darkgreen"), 
+                    interpolate = "linear")
 
-dist_palette = colorNumeric(palette = "YlOrBr", 
-                              domain = wgs_trails$miles)
-
-
+density_palette = colorNumeric(palette = my_ramp, 
+                              domain = log(parks$trees_per_acre))
 
 map <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
   addProviderTiles(provider = providers$Stamen.TonerLite) %>%
-  addPolygons(data = wgs_buffer,
-              fillColor = "black",
-              fillOpacity = 0.6,
-              stroke = FALSE) %>%
-  addPolylines(data = wgs_trails,
-               popup = ~popup_txt,
-               color = ~dist_palette(miles),
-               opacity = 1,
-               highlightOptions = highlightOptions(color = "green")) %>%
-  addLegend(colors = "black",
-            opacity = 0.6,
-            labels = "Area within 1/4 mile of transit",
-            position = "bottomright") %>%
-  addLegend(pal = dist_palette, 
-            values = wgs_trails$miles,
-            title = "Miles to nearest transit stop",
-            opacity = 1,
-            position = "bottomright") %>%
-  addLegend(title = "Trails and Transit",
-            colors = rep("white", 6),
-            labels = c("How accessible is San Luis Obispo’s",
-                       "trail system by public transit? This map",
-                       "shows that while much of the city’s urban",
-                       "trails are with a quarter mile of a transit",
-                       "stop, a few remain accessible primarily by",
-                       "private car"),
-            position = "topleft") %>%
+  addPolygons(data = parks, fillColor = ~density_palette(log(trees_per_acre)),
+                   stroke = FALSE,
+             fillOpacity = 0.6,
+             highlightOptions = highlightOptions(fillOpacity = 0.9),
+             popup = ~popup_text)  %>%
+  addLegend(data = parks,
+            pal = density_palette, 
+            values = ~log(trees_per_acre),
+            labFormat = labelFormat(transform = function(x) exp(x),
+                                    digits = 1),
+            title = "Number of trees per acre",
+            position = "bottomleft") %>%
   addLegend(title = "Sources and acknowledgments",
-            colors = rep("white", 8),
-            labels = c("Transit stop locations from SLO Transit's",
-                       "and SLO RTA's GTFS feeds. Trail locations",
-                       "from City of San Luis Obispo GIS Hub and ",
-                       "SLO County Open Data.",
+            colors = rep("white", 3),
+            labels = c("Park boundaries and tree locations from Analyze Boston (data.boston.gov).",
                        "",
-                       "My classmate, Rachel Meltzer, showed me",
-                       "how to hack a legend control to annotate a",
-                       "Leaflet map."),
-            position = "bottomleft") 
-
+                       "My classmate, Marin Bradshaw, showed me how to define a leaflet color ramp."),
+            position = "bottomright") %>%
+  addLegend(title = "How Green is Boston's Open Space?",
+            colors = rep("white", 6),
+            labels = c("Boston is home to more than 6,700 acres of",
+                       "open space, but there is a wide variation in ",
+                       "the density of trees within these spaces.",
+                       "This map shows a greater density of public ",
+                       "trees per acre in the central parts of the ",
+                       "city than on the periphery."),
+            position = "topleft") 
 map
 
-htmlwidgets::saveWidget(map, file = here("a4-files",
+htmlwidgets::saveWidget(map, file = here("a6-files",
                                          "Examples",
-                                         "slo-trails.html"))
+                                         "park-map.html"))
